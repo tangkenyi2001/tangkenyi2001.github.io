8a670e5765a8aa976adefc6f7b586c9fdce6ae18
tangkenyi2001 <tangkenyi2001@gmail.com>
2026-01-29 21:01:45 +0800

    update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tangkenyi2001', // Usually your GitHub org/user name.
   projectName: 'tangkenyi2001.github.io', // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
